f5e4720a63c0c34bf1d30d1db26b56a36032dac1
Slevkelebr <Slevkelebr@yandex.ru>
2020-12-03 21:21:47 +0500

    2. CandidateSerlvet. Создание кандидата.

--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -45,4 +45,9 @@ public class Store {
         posts.put(post.getId(), post);
     }
 
+    public void save(Candidate candidate) {
+        candidate.setId(POST_ID.incrementAndGet());
+        candidates.put(candidate.getId(), candidate);
+    }
+
 }
